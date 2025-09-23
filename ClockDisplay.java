630708314aaf5a8191bd3c45116cb87123915a0b
shrnzz <114313101+shrnzz@users.noreply.github.com>
2025-09-22 20:24:05 -0400

    Update header comment
    reminder of what needs to be done on this branch to complete this half of the assignment

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,9 +1,13 @@
 
 /**
  * The ClockDisplay class implements a digital clock display for a
- * European-style 24 hour clock. The clock shows hours and minutes. The 
- * range of the clock is 00:00 (midnight) to 23:59 (one minute before 
- * midnight).
+ * American-style 12 hour clock. The clock shows hours and minutes. The 
+ * range of the clock internally is 00:00 (midnight) to 23:59 (one minute before 
+ * midnight). But the clock should display 12:00AM - 11:59AM and 
+ * 12:00PM - 11:59PM.
+ * 
+ * The code needs to translate the hours to the equivalent 12 hour time block 
+ * and must append the appropriate meridian designation (AM or PM).
  * 
  * The clock display receives "ticks" (via the timeTick method) every minute
  * and reacts by incrementing the display. This is done in the usual clock
