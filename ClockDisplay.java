aa8b74a4d42d2de8ecb9e83a726ee6790b77f52c
shrnzz <114313101+shrnzz@users.noreply.github.com>
2025-09-27 17:26:40 -0400

    updated class and constructor

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -16,14 +16,15 @@
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Sahar Naz
+ * @version 2025-09-27
  */
 public class ClockDisplay
 {
     private NumberDisplay hours;
     private NumberDisplay minutes;
     private String displayString;    // simulates the actual display
+    private String meridian;        // AM or PM
     
     /**
      * Constructor for ClockDisplay objects. This constructor 
@@ -33,6 +34,9 @@ public class ClockDisplay
     {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
+        hours.setValue(0);
+        minutes.setValue(0);
+        meridian = "AM";
         updateDisplay();
     }
 
